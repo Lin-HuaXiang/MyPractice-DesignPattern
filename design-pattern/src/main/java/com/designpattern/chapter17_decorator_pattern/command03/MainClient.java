71baefdffb9b8239b091d4ac228f2be2a1a26df3
LinHuaXiang <jhuarreiongqiao1>
2019-07-15 08:23:01 +0800

    daily-branch daily commit 20190715

--- design-pattern/src/main/java/com/designpattern/chapter17_decorator_pattern/command03/MainClient.java
+++ design-pattern/src/main/java/com/designpattern/chapter17_decorator_pattern/command03/MainClient.java
@@ -13,7 +13,7 @@ public class MainClient {
         // component变成了concreteDecorator1
         component = new ConcreteDecorator3(component);
         // 三个component的地址不是同一个，因此子类实现方法都执行，他们不会相互影响，都是指针的指向
-        // super.operator上一个通过构造器传递的方法中的operator而不是指代父类的方法
+        // super.operator是指代父类的方法，但是父类的却是调用了传递过来的参数的operator，才能做到上一个子类实现方法的调用
         component.operate();
     }
 }
