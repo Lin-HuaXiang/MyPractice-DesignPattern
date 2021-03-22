a912b23f06f7fe0a7c04e5fb79f5d5c44960aa13
linhuaxaing <547832803@qq.com>
2021-03-22 15:16:35 +0800

    on 20210322, my practice data analysis

---
+++ design-pattern-index-analysis/src/main/java/com/example/designpatternindexanalysis/DesignPatternIndexAnalysisApplication.java
@@ -0,0 +1,17 @@
+package com.example.designpatternindexanalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternIndexAnalysisApplication {
+
+
+	
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignPatternIndexAnalysisApplication.class, args);
+	}
+
+
+}
---
+++ design-pattern-index-analysis/src/test/java/com/example/designpatternindexanalysis/DesignPatternIndexAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.designpatternindexanalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternIndexAnalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
